eadab642010f4ea0bec4adb6d052eb24251aac72
charanjeet singh <ggumber1998@gmail.com>
2024-02-04 22:46:03 +0530

    changes in background

--- src/components/Temp.jsx
+++ src/components/Temp.jsx
@@ -1,9 +1,12 @@
-import { useEffect, useState } from 'react'
+
+import { useEffect, useState } from 'react';
 import Select from 'react-select';
-const Temp = ()=>{
-    const [City, SetCity] = useState('Muktsar')
-    const [Temp, SetTemp] = useState(null)
-    const [Icon, SetIcon] = useState(null)
+
+const Temp = () => {
+    const [City, SetCity] = useState('Muktsar');
+    const [Temp, SetTemp] = useState(null);
+    const [Icon, SetIcon] = useState(null);
+    const [BackgroundColor, setBackgroundColor] = useState('black'); // Default background color
     const [allcities, Setallcities] = useState([
         "Abidjan",
 "Abu Dhabi",
@@ -377,24 +380,20 @@ const Temp = ()=>{
 "Yokohama",
 "Zagreb",
     ])
-
-    useEffect(()=>{
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${City},&units=metric&wind.speed.unit=imper&appid=dbdc1cf4134f3ac6426d8884d7f388ab`)
-        .then((response)=>{
-            return response.json()
-        }).then((response)=>{
-                
-            if(response){
-
-                console.log(response)
-                SetTemp(response.main)
-                SetIcon(response.weather)
-            }
-        
-           
-        })
-    },[City])
     
+
+    useEffect(() => {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${City},&units=metric&wind.speed.unit=imper&appid=dbdc1cf4134f3ac6426d8884d7f388ab`)
+            .then((response) => response.json())
+            .then((response) => {
+                if (response) {
+                    console.log(response);
+                    SetTemp(response.main);
+                    SetIcon(response.weather);
+                }
+            });
+    }, [City]);
+
     useEffect(() => {
         // Play background audio when component mounts
         const audio = new Audio('../mp3/thunder-12.mp3'); // Replace 'your-audio-file.mp3' with the actual file path
@@ -403,150 +402,93 @@ const Temp = ()=>{
         // Cleanup function to stop audio when component unmounts
         return () => {
             audio.pause();
-            audio.currentTime =0;
+            audio.currentTime = 0;
         };
     }, [City]);
 
-    const InputData =(event)=>{
-        SetCity(event.target.value)
-       
-   }
+    const InputData = (event) => {
+        SetCity(event.target.value);
+    };
 
-   const handleChange = (selectedOption) => {
-    SetCity(selectedOption.value);
+    const handleChange = (selectedOption) => {
+        SetCity(selectedOption.value);
+
+        // Set background color based on the selected city
+        const selectedBackgroundColor = getRandomColor();
+        setBackgroundColor(selectedBackgroundColor);
+    };
+
+    const getRandomColor = () => {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    };
+
+    const customStyles = {
+        option: (provided, state) => ({
+            ...provided,
+            backgroundColor: state.isFocused ? '#f0f0f0' : 'white', // Change the background color when focused
+            color: state.isSelected ? 'white' : 'black', // Change the text color when selected
+        }),
+    };
+
+    return (
+        <div className="container">
+            <div className="container main w-50 " style={{ backgroundColor: BackgroundColor }}>
+                <div className="row row1 mt-5 pt-5">
+                    <label htmlFor="exampleDataList" className="form-label">
+                        Search City
+                    </label>
+                    <Select
+                        value={{ label: City, value: City }}
+                        onChange={handleChange}
+                        options={allcities.map((city) => ({ label: city, value: city }))}
+                        styles={customStyles} // Apply the custom styles
+                    />
+
+                    <datalist id="datalistOptions">
+                        {console.log(allcities, '====')}
+                        {allcities.map((city, index) => (
+                            <option key={index} value={city}>
+                                {city}
+                            </option>
+                        ))}
+                    </datalist>
+                    {!Temp ? (
+                        <p>No data found</p>
+                    ) : (
+                        <div>
+                            <h2 className="text-center py-3">
+                                <i className="fa-solid fa-location-dot"></i> {City} {Math.round(Temp.temp)}{' '}
+                                <span>
+                                    <sup>o</sup>C
+                                </span>
+                            </h2>
+                            <img
+                                src={`http://openweathermap.org/img/wn/${Icon[0].icon}@2x.png`}
+                                alt="icon"
+                                className="d-block mx-auto"
+                            />
+                            <p className="text-center ps-3" style={{ marginTop: '-15px' }}>
+                                {Icon[0].main}
+                            </p>
+                            <h5 className="pt-5">
+                                Min Temp: {Temp.temp_min}
+                                <sup>o</sup>C | Max Temp: {Temp.temp_max}
+                                <sup>o</sup>C
+                            </h5>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
 };
-
-const customStyles = {
-    option: (provided, state) => ({
-        ...provided,
-        backgroundColor: state.isFocused ? '#f0f0f0' : 'white', // Change the background color when focused
-        color: state.isSelected ? 'white' : 'black', // Change the text color when selected
-        
-    }),
-};
-
-const audio =("../mp3/thunder-12.mp3")
-
-
-    return(
-
-       
-
-//             <div className="container tempc " >
-//             <div className="row main-cont" >
-//             {console.log(allcities, "allcities")}
-//                 <div className="col-md-12">
-//                 <label htmlFor="exampleDataList" className="form-label">Search City</label>
-//                 <Select
-                
-//                         value={{ label: City, value: City }}
-//                         onChange={handleChange}
-//                         options={allcities.map((city) => ({ label: city, value: city,  }))}
-//                         styles={customStyles} // Apply the custom styles
-//                     />
-//                 </div>
-//                 <div className="row">
-//                     <div className="col-md-12">
-              
-                    
-//                      <datalist id="datalistOptions">
-// {console.log(allcities, "====")}
-//                         {allcities.map((city, index)=> (
-                          
-//                           <option key={index} value={city}>
-//                           {city}
-//                       </option>
-
-//                         ))}
-
-
-                        
-                        
-//                      </datalist>
-                 
-//                     </div>
-//                     <div className="row">
-//                         <div className="col-md-12">
-//                         { !Temp ? ( 
-//                     <p>No data found</p>
-                   
-//                 ) :( 
-//                     <div className='clearfix'>
-//                 <div>
-//                     <h2 className='text-center py-3'><i className="fa-solid fa-location-dot"></i> {City} {Math.round(Temp.temp)} <span><sup>o</sup>C</span>
-//                     </h2>
-//                     <div className=''>
-//                     <img src={`http://openweathermap.org/img/wn/${Icon[0].icon}@2x.png`} alt='icon' className='d-block mx-auto'/>
-//                     <p className='text-center ps-3' style={{marginTop:'-15px'}}>{Icon[0].main}</p>
-//                     </div>
-//                     <div className='text-center '>
-//                     <p>Min Temp: {Temp.temp_min}<sup>o</sup>C | Max Temp: {Temp.temp_max}<sup>o</sup>C</p>
-//                 </div>
-                   
-                   
-               
-                
-//                 </div>
-//                 </div>
-                
-                
-               
-//                 )
-//                 }
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-
-
-// </div>
-<div className="container ">
-<div className="container main w-50 ">
-     <div className="row row1 mt-5 pt-5">
-     <label htmlFor="exampleDataList" className="form-label">Search City</label>
-                 <Select
-                
-                         value={{ label: City, value: City }}
-                         onChange={handleChange}
-                         options={allcities.map((city) => ({ label: city, value: city,  }))}
-                         styles={customStyles} // Apply the custom styles
-                     />
-                     
-                      <datalist id="datalistOptions">
-                            {console.log(allcities, "====")}
-                         {allcities.map((city, index)=> (
-                          
-                           <option key={index} value={city}>
-                           {city}
-                       </option>
-
-                         ))}
-                       
-                      </datalist>
-                      { !Temp ? ( 
-                     <p>No data found</p>
-                   
-                 ):( 
-                
-                      <div>
-                     <h2 className='text-center py-3'><i className="fa-solid fa-location-dot"></i> {City} {Math.round(Temp.temp)} <span><sup>o</sup>C</span>
-                     </h2>
-                    
-                     <img src={`http://openweathermap.org/img/wn/${Icon[0].icon}@2x.png`} alt='icon' className='d-block mx-auto'/>
-                     <p className='text-center ps-3' style={{marginTop:'-15px'}}>{Icon[0].main}</p>
-                     
-                     
-                     <h5 className='pt-5'>Min Temp: {Temp.temp_min}<sup>o</sup>C | Max Temp: {Temp.temp_max}<sup>o</sup>C</h5>
-                 
-                     </div>   
-                        
-                 )}    
-     </div>
-   
-</div>
-</div>
-  )
-}
-
 
 export default Temp;
+
+
+
